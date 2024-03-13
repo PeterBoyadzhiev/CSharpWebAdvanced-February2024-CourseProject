e8937dd8919dbba03021b6236e98e0f7ec2d631c
Peter <Peter@DESKTOP-8EI1DI2>
2024-03-13 20:15:50 +0200

    Add project files.

---
+++ AppConstantsCommon/AppConstantsGeneral.cs
@@ -0,0 +1,7 @@
+﻿namespace ExploreUmami.Common
+{
+    public static class AppConstantsGeneral
+    {
+        public const int ReleaseYear = 2024;
+    }
+}
---
+++ ExploreUmami.web/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using ExploreUmami.Web.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ExploreUmami.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
