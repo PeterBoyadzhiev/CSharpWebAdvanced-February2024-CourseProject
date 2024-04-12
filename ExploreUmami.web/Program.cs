5070ba94036d4b6e2bbc795f4f9402a1c7fad8ae
Petar Boyadzhiev <peterboyadzhiev@students.softuni.bg>
2024-04-12 22:47:39 +0300

    Add custom error pages

--- ExploreUmami.web/Program.cs
+++ ExploreUmami.web/Program.cs
@@ -51,7 +51,8 @@ namespace ExploreUmami.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 app.UseHsts();
             }
 
